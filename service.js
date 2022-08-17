458f5520a1d143f19f206f101774119a7ef55a97
Abinaya-251 <107998422+Abinaya-251@users.noreply.github.com>
2022-08-17 11:54:29 +0530

    Update service.js

--- service.js
+++ service.js
@@ -2,9 +2,10 @@
 const express = require('express');
 // Constants
 const PORT = 3000;
+const HOST = 0.0.0.0;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Docker and Nodejs');
+  res.send(<h2 style="color:Purple"> Docker and Nodejs appdemp </h2>');
 });
 app.listen(PORT, HOST);
 console.log(`Running on ${PORT}`);
